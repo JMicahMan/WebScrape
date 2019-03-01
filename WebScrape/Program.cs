6bcc7698e9253e68a5106b9ea1ea94af1df28758
JMicahMan <j.micah.manzanares@gmail.com>
2019-03-01 11:38:25 -0600

    Add project files.

---
+++ WebScrape/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(Console.BufferWidth, 2000);
+            var url = "https://github.com/JMicahMan/Weapon-Customizer/wiki";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+            Console.WriteLine(html.Result);
+            Console.ReadLine();
+        }
+    }
+}
