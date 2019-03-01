5f2c8a8855200623109ad6e1e9243c22843246be
JMicahMan <j.micah.manzanares@gmail.com>
2019-03-01 17:53:39 -0600

    eod

--- WebScrape/Program.cs
+++ WebScrape/Program.cs
@@ -1,3 +1,4 @@
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,28 @@ namespace WebScrape
         static void Main(string[] args)
         {
             Console.SetBufferSize(Console.BufferWidth, 2000);
-            var url = "https://github.com/JMicahMan/Weapon-Customizer/wiki";
-            var httpClient = new HttpClient();
-            var html = httpClient.GetStringAsync(url);
-            Console.WriteLine(html.Result);
+            GetHtmlAsync();
+
+
+
             Console.ReadLine();
         }     
+
+
+        private static async void GetHtmlAsync()
+        {
+            var url = "https://github.com/JMicahMan/Weapon-Customizer/wiki";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var HDoc = new HtmlDocument();
+
+            HDoc.LoadHtml(html);
+
+            var WeaponList = HDoc.DocumentNode.Descendants("markdown-body").Where(node => node.GetAttributeValue("p", "").Equals("ListViewInner")).ToList();
+
+            Console.WriteLine();
+        }
+
     }
 }
